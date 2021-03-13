5ebc2474d8720d87569cf414bd0275010edf8f19
iopi <lionel.auroux@gmail.com>
2021-03-13 09:52:11 +0100

    Add packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='TreeMatching',
+      version='0.1',
+      description='The module treematching allow you to write patterns to match subtrees in any trees of python objects.',
+      author='Lionel Auroux',
+      author_email='lionel.auroux@gmail.com',
+      url='https://github.com/LionelAuroux/treematching',
+     )
